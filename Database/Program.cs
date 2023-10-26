fe805b26361a17017ad388435dc820a9337dea45
Abhinaya148 <83582857+Abhinaya148@users.noreply.github.com>
2023-10-26 23:27:53 +0530

    1

---
+++ Database/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Database
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataReader reader = new DataReader();
+            Console.ReadLine();
+        }
+    }
+}
